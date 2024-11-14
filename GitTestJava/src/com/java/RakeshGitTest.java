440ee54c4881aa64238dae9306f7162f64eb108e
Soni <Soni@DESKTOP-7H5LP7U>
2024-11-14 18:48:17 +0530

    My first commit

---
+++ GitTestJava/src/com/java/RakeshGitTest.java
@@ -0,0 +1,12 @@
+package com.java;
+
+import java.util.Date;
+
+public class RakeshGitTest {
+	
+	public static void main(String args[]) {
+		System.out.println("Hi Rakesh I am from Eclipse");
+		System.out.println("Tadays Date =" +new Date());
+	}
+
+}
