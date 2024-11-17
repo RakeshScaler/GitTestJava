3320a0ceda86ffa30c5bf4d2d16a2741aa38bf31
Soni <Soni@DESKTOP-7H5LP7U>
2024-11-17 21:28:13 +0530

    Deleted and added

---
+++ GitTestJava/src/demo/TestRakesh.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class TestRakesh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Rakesh I am from Eclipse");
+
+	}
+
+}
