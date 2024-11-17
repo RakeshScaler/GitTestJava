e5ff6a05bfea97dd1c7f3973d6c7651cdd1669b6
Soni <Soni@DESKTOP-7H5LP7U>
2024-11-17 21:20:12 +0530

    added

--- GitTestJava/src/com/java/RakeshGitTest.java
+++ GitTestJava/src/com/java/RakeshGitTest.java
@@ -7,6 +7,8 @@ public class RakeshGitTest {
 	public static void main(String args[]) {
 		System.out.println("Hi Rakesh I am from Eclipse");
 		System.out.println("Tadays Date =" +new Date());
+		
+		System.out.println("lagi padi hai");
 	}
 
 }
